4a3bf7e42cbabe4a853fb71d4bb081913970e02c
Bo Chen <bo.chen@kindredgroup.com>
2017-05-19 15:48:33 +1000

    seperate frankel-rs-mock to a new project

--- frankel-mock-server/resolvers/index.js
+++
@@ -1,6 +0,0 @@
-export { default as daysResolver } from './days';
-export * from './events';
-export { default as raceResolver } from './race';
-export { default as meetingResolver } from './meeting';
-export { default as marketsResolver } from './market';
-export { default as competitorResolver } from './competitor';
